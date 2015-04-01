3778820c8e7f02b29cb2839fecacab87cb73a492
John Stark <star5859@stthomas.edu>
2015-03-31 20:53:38 -0600

    added john and fred
    added john and fred

--- sql/create-users.sql
+++ sql/create-users.sql
@@ -35,6 +35,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`user_id`, `user_name`, `first_name`, `last_name`, `password`, `salt`, `email`) VALUES
 (1, 'mike', 'Mike', 'Palmer', '0aed4b7874c4705f1f23129cdf8f5d68844572bd3e13a508143316172c277d2b', '71d5f2632e1ff090', 'mwpalmer@hotmail.com');
+INSERT INTO `users` (`user_id`, `user_name`, `first_name`, `last_name`, `password`, `salt`, `email`) VALUES
+(2, 'john', 'John', 'Stark', 'b9dc041178a12be3db7c9d49f2b924a2a126bd01d2d1ed293a0db2aef182f84e', '6f42f5783ceba9d', 'js1235813@gmail.com');
+INSERT INTO `users` (`user_id`, `user_name`, `first_name`, `last_name`, `password`, `salt`, `email`) VALUES
+(3, 'fred', 'Fred', 'Flintstone', 'e5e1f158b16522846f6caa3731679c8231b70f556917504b249f035381bab243', '5b8baedb5cee6fd0', 'dictabird@hotmail.com');
 
 --
 -- Indexes for dumped tables
