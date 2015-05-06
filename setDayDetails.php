034baf40bd341b6dafe476e42f9d81f3141287e8
Mike Palmer <mwpalmer@hotmail.com>
2015-05-06 15:12:32 -0500

    fix reference to localhost
    fix reference to localhost and fix indentation

--- setDayDetails.php
+++ setDayDetails.php
@@ -1,40 +1,39 @@
 <?php
 require("config.php");
-?>
 
-  <?php
-    $vacationPlanId = $_SESSION['currentVacationPlanId'];
-    $dayDate = $_GET['dayDate'];
-    $travelTime = $_GET['travelTime'];
-    $travelDistance = $_GET['travelDistance'];
-    $startingLocation = $_GET['startingLocation'];
-    $endingLocation = $_GET['endingLocation'];
-    $morningActivity = $_GET['morningActivity'];
-    $afternoonActivity = $_GET['afternoonActivity']; 
-    $eveningActivity = $_GET['eveningActivity'];
-    $lodging = $_GET['lodging']; 
-    $morningStatus = $_GET['morning_status'];
-    $afternoonStatus = $_GET['afternoon_status'];
-    $eveningStatus = $_GET['evening_status'];
-    $lodgingStatus = $_GET['lodging_status'];
+$vacationPlanId = $_SESSION['currentVacationPlanId'];
+$dayDate = $_GET['dayDate'];
+$travelTime = $_GET['travelTime'];
+$travelDistance = $_GET['travelDistance'];
+$startingLocation = $_GET['startingLocation'];
+$endingLocation = $_GET['endingLocation'];
+$morningActivity = $_GET['morningActivity'];
+$afternoonActivity = $_GET['afternoonActivity'];
+$eveningActivity = $_GET['eveningActivity'];
+$lodging = $_GET['lodging'];
+$morningStatus = $_GET['morning_status'];
+$afternoonStatus = $_GET['afternoon_status'];
+$eveningStatus = $_GET['evening_status'];
+$lodgingStatus = $_GET['lodging_status'];
 
-    $colorQuery = "
+$colorQuery = "
         SELECT
           HEXcolor,
           status_id
         FROM status_def
     ";
-    try{
-        $colorStmt = $db->prepare($colorQuery);
-        $colorResult = $colorStmt->execute();
-    }
-    catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
-    $colorArray = array();
-    while($colorRow = $colorStmt->fetch()){
-        $colorArray[$colorRow['HEXcolor']] = $colorRow['status_id'];
-    }
+try {
+    $colorStmt = $db->prepare($colorQuery);
+    $colorResult = $colorStmt->execute();
+} catch (PDOException $ex) {
+    die("Failed to run query: " . $ex->getMessage());
+}
+$colorArray = array();
+while ($colorRow = $colorStmt->fetch()) {
+    $colorArray[$colorRow['HEXcolor']] = $colorRow['status_id'];
+}
 
-    $query = "
+$query = "
         UPDATE  `vacation_plan`  
         SET `day_date` =  :dayDate,
             `travel_time` =  :travelTime,
@@ -51,30 +50,30 @@ require("config.php");
             `lodging_status` =  :lodgingStatus
         WHERE vacation_plan_id = :vacationPlanId; ";
 
-    $query_params = array(
-        ':dayDate' => $dayDate,
-        ':travelTime' => $travelTime,
-        ':travelDistance' => $travelDistance,
-        ':startingLocation' => $startingLocation,
-        ':morningActivity' => $morningActivity,
-        ':endingLocation' => $endingLocation,
-        ':morningStatus' => $morningStatus,
-        ':afternoonActivity' => $afternoonActivity,
-        ':afternoonStatus' => $afternoonStatus,
-        ':eveningActivity' =>  $eveningActivity,
-        ':eveningStatus' => $eveningStatus ,
-        ':lodging' => $lodging,
-        ':lodgingStatus' => $lodgingStatus,
-        ':vacationPlanId' => $vacationPlanId 
-    );
+$query_params = array(
+    ':dayDate' => $dayDate,
+    ':travelTime' => $travelTime,
+    ':travelDistance' => $travelDistance,
+    ':startingLocation' => $startingLocation,
+    ':morningActivity' => $morningActivity,
+    ':endingLocation' => $endingLocation,
+    ':morningStatus' => $morningStatus,
+    ':afternoonActivity' => $afternoonActivity,
+    ':afternoonStatus' => $afternoonStatus,
+    ':eveningActivity' => $eveningActivity,
+    ':eveningStatus' => $eveningStatus,
+    ':lodging' => $lodging,
+    ':lodgingStatus' => $lodgingStatus,
+    ':vacationPlanId' => $vacationPlanId
+);
 
-try{
+try {
     $stmt = $db->prepare($query);
     $result = $stmt->execute($query_params);
+} catch (PDOException $ex) {
+    die("Failed to run query: " . $ex->getMessage());
 }
+header("Location: vacationSummary.php");
 
-    catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
-    header( "Location: http://localhost/vacation/vacationSummary.php");
-  
-    ?>
+?>
   
